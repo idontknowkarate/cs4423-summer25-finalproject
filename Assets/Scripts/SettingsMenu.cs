a38e76fb2bbc2b91caa48f178f0589145a83637f
idontknowkarate <josephjbaca@gmail.com>
2025-08-07 18:06:33 -0500

    Final project polish: finishing touches to UI

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,36 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
-using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [Header("Audio")]
     public AudioMixer audioMixer;
+    public Slider masterSlider, musicSlider, sfxSlider;
 
+    [Header("Resolution")]
     public TMP_Dropdown resolutionDropdown;
-    public Slider masterVolumeSlider;
-    public Slider sfxVolumeSlider;
     public Toggle fullscreenToggle;
-    public GameObject confirmPopup;
-    public AudioClip buttonClickClip;
 
     private Resolution[] resolutions;
-    private float originalMasterVol;
-    private float originalSFXVol;
-    private int originalResIndex;
-    private bool originalFullscreen;
+    private List<Resolution> uniqueResolutions = new List<Resolution>();
+    private int currentResolutionIndex = 0;
 
     void Start()
     {
-        // ------- RESOLUTION SETUP -------
         resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        var options = new System.Collections.Generic.List<string>();
-        int currentResolutionIndex = 0;
-
         HashSet<string> seenResolutions = new HashSet<string>();
+        List<string> options = new List<string>();
 
         for (int i = 0; i < resolutions.Length; i++)
         {
@@ -40,59 +32,24 @@ public class SettingsMenu : MonoBehaviour
             {
                 seenResolutions.Add(option);
                 options.Add(option);
-            }
+                uniqueResolutions.Add(resolutions[i]);
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = options.Count - 1;
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = options.Count - 1;
+                }
             }
         }
 
+        resolutionDropdown.ClearOptions();
         resolutionDropdown.AddOptions(options);
-
-        int savedResIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
-        resolutionDropdown.value = savedResIndex;
+        resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
         resolutionDropdown.RefreshShownValue();
-        SetResolution(savedResIndex);
 
-        // ------- VOLUME SETUP -------
-        float defaultVolume = 0.5f;
+        fullscreenToggle.isOn = Screen.fullScreen;
 
-        float savedMaster = PlayerPrefs.GetFloat("MasterVolume", defaultVolume);
-        float savedSFX = PlayerPrefs.GetFloat("SFXVolume", defaultVolume);
-
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(savedMaster) * 20);
-        audioMixer.SetFloat("SFXVolume", Mathf.Log10(savedSFX) * 20);
-
-        if (masterVolumeSlider) masterVolumeSlider.value = savedMaster;
-        if (sfxVolumeSlider) sfxVolumeSlider.value = savedSFX;
-
-        // ------- FULLSCREEN SETUP -------
-        bool savedFullscreen = PlayerPrefs.GetInt("Fullscreen", Screen.fullScreen ? 1 : 0) == 1;
-        Screen.fullScreen = savedFullscreen;
-        if (fullscreenToggle) fullscreenToggle.isOn = savedFullscreen;
-
-        originalMasterVol = masterVolumeSlider.value;
-        originalSFXVol = sfxVolumeSlider.value;
-        originalResIndex = resolutionDropdown.value;
-        originalFullscreen = fullscreenToggle.isOn;
-    }
-
-    bool SettingsHaveChanged()
-    {
-        return
-            masterVolumeSlider.value != originalMasterVol ||
-            sfxVolumeSlider.value != originalSFXVol ||
-            resolutionDropdown.value != originalResIndex ||
-            fullscreenToggle.isOn != originalFullscreen;
-    }
-
-    public void SetResolution(int index)
-    {
-        Resolution res = resolutions[index];
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
-        PlayerPrefs.SetInt("ResolutionIndex", index);
+        LoadVolumeSettings();
     }
 
     public void SetMasterVolume(float volume)
@@ -101,10 +58,26 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetFloat("MasterVolume", volume);
     }
 
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
     public void SetSFXVolume(float volume)
     {
         audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetResolution(int index)
+    {
+        if (index < 0 || index >= uniqueResolutions.Count) return;
+
+        Resolution selectedRes = uniqueResolutions[index];
+        Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen; // Optional
+        Screen.SetResolution(selectedRes.width, selectedRes.height, Screen.fullScreenMode);
+        PlayerPrefs.SetInt("ResolutionIndex", index);
     }
 
     public void SetFullscreen(bool isFullscreen)
@@ -113,81 +86,36 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
     }
 
-    public void ResetToDefault()
+    public void ApplySettings()
     {
-        PlayButtonSFX();
-
-        PlayerPrefs.SetFloat("MasterVolume", 0.5f);
-        PlayerPrefs.SetFloat("SFXVolume", 0.5f);
-        PlayerPrefs.SetInt("ResolutionIndex", 0);
-        PlayerPrefs.SetInt("Fullscreen", 1);
-
-        SetMasterVolume(0.5f);
-        SetSFXVolume(0.5f);
-        SetResolution(0);
-        SetFullscreen(true);
-
-        if (masterVolumeSlider) masterVolumeSlider.value = 0.5f;
-        if (sfxVolumeSlider) sfxVolumeSlider.value = 0.5f;
-        if (resolutionDropdown) resolutionDropdown.value = 0;
-        if (fullscreenToggle) fullscreenToggle.isOn = true;
-    }
-
-    public void ShowConfirmPopup()
-    {
-        if (SettingsHaveChanged())
-        {
-            confirmPopup.SetActive(true);
-        }
-        else
-        {
-            CloseSettingsPanel();
-        }
-    }
-
-    public void ConfirmSettings()
-    {
-        // apply all settings
-        SetMasterVolume(masterVolumeSlider.value);
-        SetSFXVolume(sfxVolumeSlider.value);
         SetResolution(resolutionDropdown.value);
         SetFullscreen(fullscreenToggle.isOn);
-
-        // save new values as "original" to prevent unnecessary prompts next time
-        originalMasterVol = masterVolumeSlider.value;
-        originalSFXVol = sfxVolumeSlider.value;
-        originalResIndex = resolutionDropdown.value;
-        originalFullscreen = fullscreenToggle.isOn;
-
-        PlayerPrefs.SetFloat("MasterVolume", originalMasterVol);
-        PlayerPrefs.SetFloat("SFXVolume", originalSFXVol);
-        PlayerPrefs.SetInt("ResolutionIndex", originalResIndex);
-        PlayerPrefs.SetInt("Fullscreen", originalFullscreen ? 1 : 0);
-
-        // hide popup and close settings
-        confirmPopup.SetActive(false);
-        CloseSettingsPanel();
     }
 
-    public void CancelConfirm()
+    public void ResetToDefault()
     {
-        confirmPopup.SetActive(false);
+        masterSlider.value = 0.75f;
+        musicSlider.value = 0.75f;
+        sfxSlider.value = 0.75f;
+
+        fullscreenToggle.isOn = true;
+        resolutionDropdown.value = currentResolutionIndex;
+
+        ApplySettings();
     }
 
-    public GameObject settingsPanel;
-    public GameObject mainPanel;
-
-    void CloseSettingsPanel()
+    void LoadVolumeSettings()
     {
-        settingsPanel.SetActive(false);
-        mainPanel.SetActive(true);
-    }
+        float master = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+        float music = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        float sfx = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
 
-    public void PlayButtonSFX()
-    {
-        if (buttonClickClip != null && SFXManager.instance != null)
-        {
-            SFXManager.instance.PlaySFXClip(buttonClickClip, transform, 1f);
-        }
+        masterSlider.value = master;
+        musicSlider.value = music;
+        sfxSlider.value = sfx;
+
+        SetMasterVolume(master);
+        SetMusicVolume(music);
+        SetSFXVolume(sfx);
     }
 }
